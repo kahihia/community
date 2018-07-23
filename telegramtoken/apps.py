46d55d1029764b8d66278c930a9d574e7d1421d8
Joseph Kahihia <info@chrystalsolution.com>
2018-07-23 08:46:04 +0300

    init

---
+++ telegramtoken/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class TelegramTokenConfig(AppConfig):
+    name = 'telegramtoken'
+    verbose_name = "Telegram Token"
---
+++ telegramtoken/settings/sample_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+NODE_IP = '127.0.0.1'
+NODE_PORT = '9718'
+NODE_USER = 'testuser'
+NODE_PWD = 'testpassword'
+
+STREAM_SMART_LICENSE = 'Telegram-license'
+STREAM_SMART_LICENSE_ATTESTATION = 'Telegram-license-attestation'
+STREAM_ISCC = 'telegramTokenJar'
